6f1a274b4a70f446a21cf04cfbe84da87301c14d
MikeRixWolfe <michaelrixwolfe@gmail.com>
2017-06-07 15:57:02 -0400

    Add config option for eps per page

--- app/views.py
+++ app/views.py
@@ -7,14 +7,16 @@ from .util import get_episodes
 @app.route('/feed', strict_slashes=False, methods=['GET'])
 def index():
     eps = get_episodes(app.config['PODCASTS_CSV'].split(','))
+    per_page = app.config.get('PER_PAGE', 10)
 
     page = request.args.get('page', type=int, default=1)
     pagination = Pagination(page=page,
+                            per_page = per_page,
                             total=len(eps),
                             css_framework='foundation',
                             prev_label='< Prev',
                             next_label='Next >')
-    page_eps = eps[(page-1)*10:page*10]
+    page_eps = eps[(page-1)*per_page:page*per_page]
 
     return render_template('index.html',
                            eps=page_eps,
